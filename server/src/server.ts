ea13d34b91d227eb0c3e6efa3c6f995a9deb2c5d
Silvio Siqueira <silviojr.siqueira@gmail.com>
2020-06-07 02:23:17 -0300

    Criando server/mobile/web nlw

---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import routes from './routes';
+import cors from 'cors';
+import path from 'path';
+import { errors } from 'celebrate';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
+
+console.log(`Server is running...`);
+app.listen(3333);
